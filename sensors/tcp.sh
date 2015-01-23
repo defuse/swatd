b6ee527b5a22ec2e0b9c4bf2b0985720ad56af52
Tobias <tobleminer@gmail.com>
2015-01-23 18:12:08 +0100

    Fixed timeout

--- sensors/tcp.sh
+++ sensors/tcp.sh
@@ -4,7 +4,7 @@ readonly PORT=80
 readonly TIMEOUT=2
 
 for i in {1..5}; do 
-    if nc -w 2 -z "$HOSTNAME" "$PORT" > /dev/null 2>&1; then
+    if nc -w "$TIMEOUT" -z "$HOSTNAME" "$PORT" > /dev/null 2>&1; then
         exit 0
     fi
 done
